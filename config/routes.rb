d6470b0e227f950f5d54085b8b63064939be7cb5
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2018-03-10 14:25:46 -0500

    created Topics scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'topics#index'
+  # you added that last line -- it makes the main index go to the topics index instead
+  resources :topics
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
